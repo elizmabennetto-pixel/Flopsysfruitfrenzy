dd43aa5c49a20e69ffabef48b4071fb0de6e3bec
elizmabennetto-pixel <elizma.bennetto@gmail.com>
2026-03-13 12:34:39 +0200

    Updated GameManager.cs with scoring and game state logic.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
-    // Main game controller logic goes here
+public class GameManager : MonoBehaviour
+{
+    private int score;
+    private string gameState;
+
+    // Method to add score
+    public void AddScore(int points)
+    {
+        score += points;
+        Debug.Log("Score: " + score);
+    }
+
+    // Method to end the game
+    public void EndGame()
+    {
+        gameState = "GameOver";
+        Debug.Log("Game Over");
+        // Additional logic for ending the game can be added here
+    }
+
+    // Method to get the current game state
+    public string GetGameState()
+    {
+        return gameState;
+    }
 }
